981c7ac5fccb69c466b82c98771ad90626765987
geocode-matt <mlindenx@gmail.com>
2020-09-29 16:38:15 -0500

    project tiles are populating

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -29,26 +29,27 @@ import ProjectCards from '../../components/Project'
 import portfolio from '../../portfolio.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Wrapper(props) {
-    return <div className="wrapper">{props.children}</div>;
-}
+// function Wrapper(props) {
+//     return <div className="wrapper">{props.children}</div>;
+// }
 
 function Portfolio () {  
 
     return (
-        <section>
-          <div className="project">
-            <hr></hr>
-          </div>
-
-          <Wrapper id="card-data">
-            {portfolio.map((project) => (
-              <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} topics={project.topics}/>
-            ))}
-          </Wrapper>
-        </section>
+      <section className="projects">
+        <hr></hr>
+        
+        <div className="row justify-content-center">
+          <div className="col-10 projects-section">
+          {portfolio.map((project) => (
+          <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} topics={project.topics}/>
+          ))}
+        </div>  
+        </div>
+      </section>
     
     );
   }
 
 export default Portfolio;
+
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,21 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Individual Card build-out using props
 function ProjectCards(props) {
     return (
-      <div className="card"> 
-        <div className="img-container">
-          <img alt={props.name} src={props.image} height="175px" />
-        </div>
-        <div className="content">
-              <p className="project-title" id="card-title">{props.name}</p>
-              <p>
-              <a href={props.github}><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Repository" id="project-icon"/></a> 
-              </p>
-              <p id="topics">
-                    ({props.topics})
-                </p>
-        </div>
-      </div>
-    );
-}
+            <div className="project-tile">
+                <img className="project-img" alt={props.name} src={props.image} />
+            </div>
+      );
+    };
   
   export default ProjectCards;
