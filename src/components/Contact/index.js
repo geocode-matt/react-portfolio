9e9e40e23d90b64c13437ed97e942a972d94acba
geocode-matt <mlindenx@gmail.com>
2020-09-30 12:06:34 -0500

    contact section built

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,81 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
-export default class Contact extends Component {
-  render() {
+function Contact(props) {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+    
+                if(!isValid) {
+                    setErrorMessage('please enter a valid email address');
+                } else {
+                    setErrorMessage('');
+                }
+
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                } 
+        }
+
+        if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
-      <section className="contact">
+      <section className="about">
       <hr></hr>
       
-      <div className="contact-form">
-        <ul>
-          <li>
-          <a href="">
-                Contact Form
-                <br />
-                <img src="" alt="" height="175px" className="app-img" />
-              </a>
-              <p>Contact Form.</p>
-          </li>
-        </ul>
+      <div className="row justify-content-center">
+        <div className="col-10" id="contact-section">
+            <div className="ml-photo-container">
+              <img className="ml-photo" src="https://raw.githubusercontent.com/geocode-matt/react-portfolio/master/src/assets/images/IMG_1909.jpg" alt="Matt Linden"/>
+            </div>
+            <div className="text-block">
+                Questions or business inquiries? <br></br>Use the form or contact me via
+                <a href="mailto:mlindenx@gmail.com" className="contact-text"> email</a> or
+                <a href="tel:817-575-9370" className="contact-text"> phone</a>.
+            </div>
+            <div id="form-container">
+        <form className="justify-content-center" id="contact-form">
+          <div className="form-input">
+              <input className="form-control" placeholder="Name" type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
+          </div>
+          <div className="form-input">
+              <input className="form-control" placeholder="Email Address" type="email"  name="email" defaultValue={email} onBlur={handleChange} />
+          </div>
+          <div className="form-input">
+              <textarea className="form-control" placeholder="Message" name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+          </div> 
+          {errorMessage && (
+          <div className="error-msg">
+              <p className="error-text">{errorMessage}</p>
+          </div>
+          )}
+
+          <div className="button-container">
+          <button data-testid='button' className="btn mt-4" type="submit" onSubmit={handleSubmit}>Send</button>
+          </div>
+      </form>
+        </div>
+      </div>  
       </div>
     </section>
     )
-  } 
-}
+    }
+
+export default Contact;
+
+
