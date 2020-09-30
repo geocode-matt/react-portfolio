5f21c21d6f054df04aea9b172e94ce7633c27607
geocode-matt <mlindenx@gmail.com>
2020-09-29 21:55:58 -0500

    still working on project card styling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,39 +1,27 @@
-// import React, {Component} from 'react';
-
-// export default class Porfolio extends Component {
-//   render() {
-//     return (
-//       <section className="portfolio">
-//       <hr></hr>
-      
-//       <div className="projects">
-//         <ul>
-//           <li>
-//           <a href="">
-//                 Password Generator
-//                 <br />
-//                 <img src="" alt="" height="175px" className="app-img" />
-//               </a>
-//               <p>Random password generator.</p>
-//           </li>
-//         </ul>
-//       </div>
-//     </section>
-//     )
-//   } 
-// }
-
-
 import React from 'react'; 
 import ProjectCards from '../../components/Project'
 import portfolio from '../../portfolio.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// function Wrapper(props) {
-//     return <div className="wrapper">{props.children}</div>;
-// }
+import passwordGenerator from '../../assets/images/password-generatorv2.png';
+import weatherDashboard from '../../assets/images/weather-dashboardv2.png';
 
 function Portfolio () {  
+  const projects = [
+    {
+    "id": 1,
+    "name": "Random Password Generator",
+    "image": passwordGenerator,
+    "github": "https://github.com/geocode-matt/password-generator",
+    "topics": "JavaScript, HTML, CSS"
+    },
+    {
+    "id": 2,
+    "name": "Weather Dashboard",
+    "image": weatherDashboard,
+    "github": "https://github.com/geocode-matt/Weather-Dashboard",
+    "topics": "JavaScript, jQuery, HTML, CSS, Bootstrap, Moment, API"
+    }
+  ];
 
     return (
       <section className="projects">
@@ -41,7 +29,7 @@ function Portfolio () {
         
         <div className="row justify-content-center">
           <div className="col-10 projects-section">
-          {portfolio.map((project) => (
+          {projects.map((project) => (
           <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} topics={project.topics}/>
           ))}
         </div>  
@@ -52,4 +40,3 @@ function Portfolio () {
   }
 
 export default Portfolio;
-
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,11 +1,17 @@
 import React from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import passwordGenerator from '../../assets/images/password-generatorv2.png';
+import weatherDashboard from '../../assets/images/weather-dashboardv2.png';
 
 // Individual Card build-out using props
 function ProjectCards(props) {
     return (
             <div className="project-tile">
-                <img className="project-img" alt={props.name} src={props.image} />
+                <h6 className="project-name">{props.name}</h6>
+                <div className="project-img-container">
+                  <img className="project-img" alt={props.name} src={props.image} />
+                </div>
+                <h6 className="project-topics">{props.topics}</h6>
             </div>
       );
     };
