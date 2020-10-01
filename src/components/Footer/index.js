385aed0d84ec0c5b8cabae591aa241393ce2b77b
geocode-matt <mlindenx@gmail.com>
2020-10-01 14:37:18 -0500

    updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ function Footer(props) {
     return (
             <div className="footer" id="footer">
                 <hr></hr>
-                <a href="https://github.com/geocode-matt"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Repository" className="icon"/></a>
-                <a href="https://www.linkedin.com/in/mlinden88/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="Repository" className="icon"/></a>
-                <a href="https://stackoverflow.com/users/14369920/matt-linden"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" className="icon"/></a>
+                <a href="https://github.com/geocode-matt" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Repository" className="icon"/></a>
+                <a href="https://www.linkedin.com/in/mlinden88/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="Repository" className="icon"/></a>
+                <a href="https://stackoverflow.com/users/14369920/matt-linden" target="_blank"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" className="icon"/></a>
                 <p>
                 &copy; matt linden  |  <a href="mailto:mlindenx@gmail.com" className="link">email me</a>  |  <a href="tel:817-575-9370" className="link">call me</a>  |  <a href="" className="link">download resume</a>
                 </p>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,9 +14,9 @@ function Header(props) {
     return (
             <div className="navbar navbar-expand-lg">
                 <div className="name-container col-lg-4">
-                <a href="https://github.com/geocode-matt"><img src="https://img.icons8.com/fluent/48/000000/github.png" target="_blank" className="icon"/></a>
-                <a href="https://www.linkedin.com/in/mlinden88/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" target="_blank" className="icon"/></a>
-                <a href="https://stackoverflow.com/users/14369920/matt-linden"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" className="icon"/></a>
+                <a href="https://github.com/geocode-matt" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/github.png" className="icon"/></a>
+                <a href="https://www.linkedin.com/in/mlinden88/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" className="icon"/></a>
+                <a href="https://stackoverflow.com/users/14369920/matt-linden" target="_blank"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" className="icon"/></a>
                 <a className="navbar-brand header-links header-name" href="#about"><span
                 onClick={() => {
                   setAboutSelected(true);
